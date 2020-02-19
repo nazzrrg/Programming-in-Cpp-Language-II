ff36fc90a263102f5ec181de0262db39bd8e980c
nazzrrg <nazarov.egor2010@yandex.ru>
2020-02-19 18:46:06 +0300

    minor corrections

---
+++ Programming in C++/lab1/cmake-build-debug/CMakeFiles/test2.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/nazzrrg/Git/Programming-in-Cpp-Language-II/Programming in C++/lab1/Matrix.cpp" "/Users/nazzrrg/Git/Programming-in-Cpp-Language-II/Programming in C++/lab1/cmake-build-debug/CMakeFiles/test2.dir/Matrix.cpp.o"
+  "/Users/nazzrrg/Git/Programming-in-Cpp-Language-II/Programming in C++/lab1/circle.cpp" "/Users/nazzrrg/Git/Programming-in-Cpp-Language-II/Programming in C++/lab1/cmake-build-debug/CMakeFiles/test2.dir/circle.cpp.o"
+  "/Users/nazzrrg/Git/Programming-in-Cpp-Language-II/Programming in C++/lab1/main.cpp" "/Users/nazzrrg/Git/Programming-in-Cpp-Language-II/Programming in C++/lab1/cmake-build-debug/CMakeFiles/test2.dir/main.cpp.o"
+  "/Users/nazzrrg/Git/Programming-in-Cpp-Language-II/Programming in C++/lab1/task_4.cpp" "/Users/nazzrrg/Git/Programming-in-Cpp-Language-II/Programming in C++/lab1/cmake-build-debug/CMakeFiles/test2.dir/task_4.cpp.o"
+  "/Users/nazzrrg/Git/Programming-in-Cpp-Language-II/Programming in C++/lab1/task_7.cpp" "/Users/nazzrrg/Git/Programming-in-Cpp-Language-II/Programming in C++/lab1/cmake-build-debug/CMakeFiles/test2.dir/task_7.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Programming in C++/lab1/cmake-build-debug/CMakeFiles/test2.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test2.dir/Matrix.cpp.o"
+  "CMakeFiles/test2.dir/circle.cpp.o"
+  "CMakeFiles/test2.dir/main.cpp.o"
+  "CMakeFiles/test2.dir/task_4.cpp.o"
+  "CMakeFiles/test2.dir/task_7.cpp.o"
+  "test2"
+  "test2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
