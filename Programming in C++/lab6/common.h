f842f5ee9004045940d926c9ff3455a2dbeabd23
nazzrrg <nazarov.egor2010@yandex.ru>
2020-02-26 01:45:19 +0300

    minor changes

--- Programming in C++/lab6/common.h
+++ Programming in C++/lab6/common.h
@@ -1,6 +1,8 @@
 //
 // Created by Егор Назаров on 25.02.2020.
 //
+#ifndef LAB4_COMMON_H
+#define LAB4_COMMON_H
 
 template <typename containerIterator>
 bool noneOf(containerIterator, containerIterator, bool);
@@ -10,3 +12,5 @@ template <typename containerIterator, typename object>
 object findNot(containerIterator, containerIterator, object);
 
 #include "common.tpp"
+
+#endif
