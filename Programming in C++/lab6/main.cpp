f951947e9c5f8b995240ef69f96cdc12409a8119
nazzrrg <nazarov.egor2010@yandex.ru>
2020-02-26 00:54:18 +0300

    work in progress

--- Programming in C++/lab6/main.cpp
+++ Programming in C++/lab6/main.cpp
@@ -4,15 +4,15 @@
 #include "common.h"
 //#include "common.tpp"
 
-bool foo(int x) {
+bool f(int x) {
     return x > 0;
 }
 
 int main() {
-    std::vector<int> A = {1, 2, 3};
-    std::set<double> B = {1., 2., 3.};
-    std::cout << noneOf(A, &foo) << " : " << noneOf(B, &foo) << "\n";
-    std::cout << isSorted(A, &foo) << " : " << isSorted(B, &foo) << "\n";
+    std::vector<int> A = {1, 5, 3};
+    std::set<double> B = {-1, 2, 3.};
+    std::cout << noneOf(A, &f) << " : " << noneOf(B, &f) << std::endl;
+    std::cout << isSorted(A, &f) << " : " << isSorted(B, &f) << std::endl;
 
-    std::cout << findNot(A.begin(), A.end(), 1) << " : " << findNot(B.begin(), B.end(), 1) << "\n";
+    std::cout << findNot(A.begin(), A.end(), 1) << " : " << findNot(B.begin(), B.end(), 1) << std::endl;
 }
